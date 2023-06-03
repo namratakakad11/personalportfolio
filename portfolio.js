50d2ea02e3f5751e6f1f9794908187b8f42ac390
Namrata kakad <134642828+namratakakad11@users.noreply.github.com>
2023-06-03 23:10:43 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+function winscrollhome(){
+    window.scroll(0,0);
+}
+function winscrollaboutme(){
+    window.scroll(0,400);
+}
+function winscrollskills(){
+    window.scroll(0,900);
+}
+function winscrollcontact(){
+    window.scroll(0,2000);
+}
